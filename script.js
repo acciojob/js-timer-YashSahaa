816bb76571531c3fdd11f579f4e70bf582508da7
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-29 23:19:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // write js code here if required
+let para = document.getElementById('timer')
+
+function currentTime(){
+	let date = new Date()
+	let hr = date.getHours()
+	let min = date.getMinutes()
+	let sec = date.getSeconds()
+	let day = date.getDay()
+	let month = date.getMonth()+1
+	let year = date.getFullYear()
+
+	para.innerText = `${day}/${month}/${year},${hr}:${min}:${sec}`
+}
+setInterval(currentTime,1000)
